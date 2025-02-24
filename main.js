457c60da53bab99b1a78b80fe7748e09a7b9d58a
Laôni A. Moreira <laonimoreira.capanema@gmail.com>
2025-02-24 13:41:18 -0300

    feat: child and about windows

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain, Menu } = require('electron/main')
 const path = require('node:path')
 
 const createWindow = () => {
@@ -12,12 +12,46 @@ const createWindow = () => {
     }
   })
 
-  win.loadFile('index.html')
+  // menu personalizado
+  Menu.setApplicationMenu(Menu.buildFromTemplate(template))
+
+  win.loadFile('./src/views/index.html')
+}
+
+// Janela sobre
+const aboutWindow = () => {
+  const about = new BrowserWindow({
+    width:360,
+    height:220,
+    // icon: './src/public/img/pc.png',
+    autoHideMenuBar: true,
+    resizable: false
+  })
+
+  about.loadFile('./src/views/sobre.html')
+}
+
+// Janela secundária
+const childWindow = () => {
+  const father = BrowserWindow.getFocusedWindow()
+  if (father) {
+    const child = new BrowserWindow({
+      width:640,
+      height:480,
+      // icon: './src/public/img/pc.png',
+      autoHideMenuBar: true,
+      resizable: false,
+      parent: father,
+      modal: true
+    })
+    child.loadFile('./src/views/child.html')
+  }
 }
 
 app.whenReady().then(() => {
   // permite a comunicação da main com renderer (IPC = inter-process communication)
   ipcMain.handle('ping', () => 'pong')
+
   createWindow()
 
   app.on('activate', () => {
@@ -32,3 +66,64 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
+
+const template = [
+  {
+    label: 'Arquivo',
+    submenu: [
+      {
+        label: 'Janela Secundária',
+        click: () => childWindow()
+      },
+      {
+        label: 'Sair',
+        click: () => app.quit(),
+        accelerator: 'Alt+F4'
+      }
+    ]
+  },
+  {
+    label: 'Exibir',
+    submenu: [
+      {
+        label: 'Recarregar',
+        role: 'reload'
+      },
+      {
+        label: 'Ferramentas do desenvolvedor',
+        role: 'toggleDevTools'
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Aplicar zoom',
+        role: 'zoomIn'
+      },
+      {
+        label: 'Reduzir',
+        role: 'zoomOut'
+      },
+      {
+        label: 'Restaurar o zoom padrão',
+        role: 'resetZoom'
+      }
+    ]
+  },
+  {
+    label: 'Ajuda',
+    submenu: [
+      {
+        label: 'docs',
+        click: () => shell.openExternal('https://www.electronjs.org/docs/latest/')
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Sobre',
+        click: () => aboutWindow()
+      }
+    ]
+  }
+]
